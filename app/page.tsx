510630ebedfe97bd606acb65b57d081fe6ed4db3
jazztinecruz <me@jazztinecruz.com>
2024-05-04 17:22:00 +0800

    chore: poster

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import get from "@/core/libraries/get";
 import { Page } from "@/core/types/react";
+import Image from "next/image";
 
 const Home: Page = async () => {
   const movies = await get.movies.popular();
@@ -7,7 +8,20 @@ const Home: Page = async () => {
   const similar = await get.movies.similar({ id: "823464" });
   const movie = await get.movie({ id: "823464" });
 
-  return <div>Home</div>;
+  if (!movie || !movies || !upcoming || !similar) return null;
+
+  return (
+    <div>
+      <div className="w-64 h-64 relative">
+        <Image
+          src={`${process.env.IMAGE_BASE_URL}${movie.poster_path}`}
+          alt="jpg"
+          fill
+          className="object-center"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
