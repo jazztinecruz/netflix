3afe2009880c47886ba543013d754759a1927594
jazztinecruz <me@jazztinecruz.com>
2024-05-05 12:05:44 +0800

    chore: cleanup url

--- core/libraries/get/movies.ts
+++ core/libraries/get/movies.ts
@@ -3,45 +3,44 @@ import api from "./api";
 
 //multiple
 export const getPopular = cache(async () => {
-  return await api({ url: "/3/movie/popular" });
+  return await api({ url: "movie/popular" });
 });
 
 export const getUpcoming = cache(async () => {
-  return await api({ url: "/3/movie/upcoming" });
+  return await api({ url: "movie/upcoming" });
 });
 
 export const getDiscover = cache(async () => {
-  return await api({ url: "/3/discover/movie" });
+  return await api({ url: "discover/movie" });
 });
 
 export const getSimilar = cache(async (id: string) => {
   if (!id) return;
-  return await api({ url: `/3/movie/${id}/similar` });
+  return await api({ url: `movie/${id}/similar` });
 });
-
 
 // single
 export const getCredits = cache(async (id: string) => {
   if (!id) return;
-  return await api({ url: `/3/movie/${id}/credits` });
+  return await api({ url: `movie/${id}/credits` });
 });
 
 export const getCertificate = cache(async (id: string) => {
   if (!id) return;
-  return await api({ url: `/3/movie/${id}/release_dates` });
+  return await api({ url: `movie/${id}/release_dates` });
 });
 
 export const getMovie = cache(async (id: string) => {
   if (!id) return;
-  return await api({ url: `/3/movie/${id}` });
+  return await api({ url: `movie/${id}` });
 });
 
 export const getVideo = cache(async (id: string) => {
   if (!id) return;
-  return await api({ url: `/3/movie/${id}/videos` });
+  return await api({ url: `movie/${id}/videos` });
 });
 
 export const getImages = cache(async (id: string) => {
   if (!id) return;
-  return await api({ url: `/3/movie/${id}/images` });
+  return await api({ url: `movie/${id}/images` });
 });
