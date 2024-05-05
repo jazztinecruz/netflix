f24f05ea50eff5b96c3c57cd5e8738f8465de192
jazztinecruz <me@jazztinecruz.com>
2024-05-05 18:56:44 +0800

    chore: hero base

---
+++ app/_components/hero/index.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <div className="h-screen bg-gradient-to-b from-black to-transparent absolute inset-0 -z-10">
+      Hero
+    </div>
+  );
+};
+
+export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
-import Collection from "@/core/components/collection";
 import { GENRE_NAME } from "@/core/enums";
 import { Page } from "@/core/types/react";
 
+import Collection from "@/core/components/collection";
+import Hero from "./_components/hero";
+
 const Home: Page = () => {
   return (
-    <div className="space-y-6">
-      <Collection title={GENRE_NAME.Fantasy} />
-      <Collection title={GENRE_NAME.Action} />
-      <Collection title={GENRE_NAME.Adventure} />
-      <Collection title={GENRE_NAME.Comedy} />
-      <Collection title={GENRE_NAME.Romance} />
+    <div className="grid">
+      <Hero />
+      <div className="space-y-6 mt-[700px]">
+        <Collection title={GENRE_NAME.Fantasy} />
+        <Collection title={GENRE_NAME.Action} />
+        <Collection title={GENRE_NAME.Adventure} />
+        <Collection title={GENRE_NAME.Comedy} />
+        <Collection title={GENRE_NAME.Romance} />
+      </div>
     </div>
   );
 };
