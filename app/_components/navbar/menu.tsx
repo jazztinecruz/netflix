93a1383af9a1e4970dab3e404da2b73dcc8cb480
jazztinecruz <me@jazztinecruz.com>
2024-05-05 14:57:17 +0800

    chore: navbar mobile

--- app/_components/navbar/menu.tsx
+++ app/_components/navbar/menu.tsx
@@ -1,18 +1,81 @@
+"use client";
+
 import { NAVMENUS } from "@/core/constants";
+import { useState } from "react";
 
 const NavMenu = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <ul className="flex items-center gap-4">
-      {NAVMENUS.map((menu, index) => {
-        const isBold = index === 0 ? "font-semibold" : "text-gray-300";
-        return (
-          <li key={menu} className={`text-sm ${isBold}`}>
-            {menu}
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <section className="relative lg:hidden w-fit">
+        <button
+          onClick={() => setOpen(!open)}
+          className="flex items-center gap-2 ">
+          <span className="text-sm">Browse</span>
+          <Triangle direction="down" />
+        </button>
+
+        {open && (
+          <button
+            onMouseLeave={() => setOpen(false)}
+            className="absolute -bottom-[370px] inset-x-2/4 -translate-x-2/4 w-72 bg-black/90 border border-gray-300/30 border-t-[3px] border-t-white rounded-t-sm">
+            <Triangle />
+            <ul className="flex flex-col items-center  justify-center text-center gap-4 text-white">
+              {NAVMENUS.map((menu, index) => {
+                const isBold = index === 0 ? "font-semibold" : "text-gray-300";
+                return (
+                  <li
+                    key={menu}
+                    className={`text-sm ${isBold} w-full py-3 hover:bg-white/10 transition-all duration-300 cursor-pointer`}>
+                    {menu}
+                  </li>
+                );
+              })}
+            </ul>
+          </button>
+        )}
+      </section>
+      <section className="hidden lg:block">
+        <ul className=" flex items-center gap-4">
+          {NAVMENUS.map((menu, index) => {
+            const isBold = index === 0 ? "font-semibold" : "text-gray-300";
+            return (
+              <li key={menu} className={`text-sm ${isBold}`}>
+                {menu}
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </>
   );
 };
 
 export default NavMenu;
+
+type TriangleProps = {
+  direction?: "up" | "down";
+};
+
+const Triangle = ({ direction = "up" }: TriangleProps) => {
+  if (direction === "down") {
+    return (
+      <div
+        className="-top-4 w-0 h-0 
+    border-l-[6px] border-l-transparent 
+    border-b-[6px] border-b-white 
+    border-r-[6px] border-r-transparent rotate-180"
+      />
+    );
+  }
+
+  return (
+    <div
+      className="absolute inset-x-2/4 -translate-x-2/4 -top-[11px] w-0 h-0 
+    border-l-[8px] border-l-transparent 
+    border-b-[8px] border-b-white   
+    border-r-[8px] border-r-transparent"
+    />
+  );
+};
