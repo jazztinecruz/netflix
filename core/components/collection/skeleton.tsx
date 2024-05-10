06cb67c1412652cd4b5769166ec205dae308bfe3
jazztinecruz <me@jazztinecruz.com>
2024-05-10 11:42:41 +0800

    fix: remove chaching

--- core/components/collection/skeleton.tsx
+++ core/components/collection/skeleton.tsx
@@ -1,13 +1,16 @@
 const ListSkeleton = () => {
   return (
-    <div className="grid grid-flow-col gap-4 relative group">
-      {Array.from({ length: 6 }).map((_, index: number) => (
-        <div
-          key={index}
-          className={`aspect-video relative rounded-md bg-gray-500 animate-pulse grid place-items-center text-lg ${
-            index === 0 && "ml-12"
-          }`}></div>
-      ))}
+    <div className="grid grid-rows-[auto,1fr] gap-2 lg:gap-3">
+      <h2 className="text-lg lg:text-xl font-medium margin">Loading</h2>
+      <div className="grid grid-flow-col gap-4 relative group">
+        {Array.from({ length: 6 }).map((_, index: number) => (
+          <div
+            key={index}
+            className={`aspect-video relative rounded-md bg-gray-500 animate-pulse grid place-items-center text-lg ${
+              index === 0 && "ml-12"
+            }`}></div>
+        ))}
+      </div>
     </div>
   );
 };
--- core/libraries/movies.ts
+++ core/libraries/movies.ts
@@ -1,32 +1,31 @@
-import { cache } from "react";
 import api from "./api";
 import { Certificate, Credit, Image, Movie, Video } from "@/core/types/data";
 
 //multiple
-export const getPopular = cache(async (): Promise<Movie[]> => {
+export const getPopular = async (): Promise<Movie[]> => {
   return await api({ url: "movie/popular" });
-});
+};
 
-export const getUpcoming = cache(async (): Promise<Movie[]> => {
+export const getUpcoming = async (): Promise<Movie[]> => {
   return await api({ url: "movie/upcoming" });
-});
+};
 
-export const getDiscover = cache(async (genreId: string): Promise<Movie[]> => {
+export const getDiscover = async (genreId: string): Promise<Movie[]> => {
   return await api({ url: `discover/movie?with_genres=${genreId}` });
-});
+};
 
-export const getSimilar = cache(async (id: string): Promise<Movie[]> => {
+export const getSimilar = async (id: string): Promise<Movie[]> => {
   return await api({ url: `movie/${id}/similar` });
-});
+};
 
 // single
-export const getCredits = cache(async (id: string): Promise<Credit[]> => {
+export const getCredits = async (id: string): Promise<Credit[]> => {
   return await api({ url: `movie/${id}/credits` });
-});
+};
 
-export const getCertificate = cache(async (id: string): Promise<string> => {
+export const getCertificate = async (id: string): Promise<string> => {
   const data: Certificate[] = await api({
-    url: `movie/${id}/release_dates`,
+    url: `movie/${id}/release_date`,
   });
 
   const certificate: string = data.find((cert) =>
@@ -34,29 +33,27 @@ export const getCertificate = cache(async (id: string): Promise<string> => {
   )?.release_dates[0].certification!;
 
   return certificate;
-});
+};
 
-export const getMovie = cache(async (id: string): Promise<Movie> => {
+export const getMovie = async (id: string): Promise<Movie> => {
   return await api({ url: `movie/${id}` });
-});
+};
 
-export const getTrailer = cache(async (id: string): Promise<Video> => {
+export const getTrailer = async (id: string): Promise<Video> => {
   const videos = await api({ url: `movie/${id}/videos` });
   const trailer = videos.filter(
     (video: Video) => video.name === "Official Trailer"
   )[0];
 
   return trailer;
-});
+};
 
-export const getImages = cache(
-  async (
-    id: string
-  ): Promise<{
-    backdrops: Image[];
-    logos: Image[];
-    posters: Image[];
-  }> => {
-    return await api({ url: `movie/${id}/images` });
-  }
-);
+export const getImages = async (
+  id: string
+): Promise<{
+  backdrops: Image[];
+  logos: Image[];
+  posters: Image[];
+}> => {
+  return await api({ url: `movie/${id}/images` });
+};
