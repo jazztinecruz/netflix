5e6cfd577e0f49f654ce563c630a959d5a539ff0
jazztinecruz <me@jazztinecruz.com>
2024-05-14 22:34:33 +0800

    fix: carousel difference in smaller and  larger screen

--- core/components/collection/carousel.tsx
+++ core/components/collection/carousel.tsx
@@ -7,6 +7,7 @@ import { useState } from 'react'
 import { Movie } from '@/core/types/data'
 
 import Backdrop from '../media/backdrop'
+import Poster from '../media/poster'
 
 type Props = {
   movies: Movie[]
@@ -36,41 +37,61 @@ const Carousel = ({ movies }: Props) => {
   })
 
   return (
-    <div className="relative overflow-hidden flex flex-col gap-3">
-      <div className="flex items-center gap-1 ml-auto mr-12">
-        {Array.from({ length: slides }).map((_, index) => (
-          <div
-            key={index}
-            className={`w-[10px] h-2 border-b-2 ${currentSlide === index ? 'border-white' : 'border-secondary'}`}
-          />
-        ))}
-      </div>
-      <div className="relative group grid grid-flow-col transition-transform duration-300 ease-in-out ">
-        <ul className={`grid grid-flow-col gap-2 ${startIndex === 0 && 'margin'}`}>
-          {visibleMovies.map((movie) => (
-            <Link key={movie.id} href={`?mid=${movie.id}`} className="w-48 lg:w-80 aspect-video rounded-md relative">
-              <Backdrop id={movie.id} />
+    <>
+      {/* smaller screen */}
+      <div className="lg:hidden overflow-y-scroll mx-4">
+        <div className="grid grid-flow-col gap-2 ">
+          {movies.map((movie) => (
+            <Link key={movie.id} href={`?mid=${movie.id}`} className="w-36 aspect-square rounded-md">
+              <Poster id={movie.id} />
             </Link>
           ))}
-        </ul>
-        {startIndex >= displayCount && (
-          <button
-            onClick={handlePrev}
-            className="absolute left-0 z-10 h-full bg-gradient-to-r from-primary to-transparent w-40 grid justify-start items-center pl-2"
-          >
-            <ChevronLeftIcon className="w-10 h-10 cursor-pointer opacity-0 group-hover:opacity-70" />
-          </button>
-        )}
-        {startIndex + displayCount < movies.length && (
-          <button
-            onClick={handleNext}
-            className="absolute right-0 h-full w-40 bg-gradient-to-r from-transparent to-primary grid justify-end items-center pr-2"
-          >
-            <ChevronRightIcon className="w-10 h-10 cursor-pointer opacity-0 group-hover:opacity-70" />
-          </button>
-        )}
+        </div>
       </div>
-    </div>
+
+      {/* larger screen */}
+      <div className="hidden lg:block">
+        <div className="relative overflow-hidden flex flex-col gap-3">
+          <div className="flex items-center gap-1 ml-auto mr-12">
+            {Array.from({ length: slides }).map((_, index) => (
+              <div
+                key={index}
+                className={`w-[10px] h-2 border-b-2 ${currentSlide === index ? 'border-white' : 'border-secondary'}`}
+              />
+            ))}
+          </div>
+          <div className="relative group grid grid-flow-col transition-transform duration-300 ease-in-out ">
+            <ul className={`grid grid-flow-col gap-2 ${startIndex === 0 && 'margin'}`}>
+              {visibleMovies.map((movie) => (
+                <Link
+                  key={movie.id}
+                  href={`?mid=${movie.id}`}
+                  className="w-48 lg:w-80 aspect-video rounded-md relative"
+                >
+                  <Backdrop id={movie.id} />
+                </Link>
+              ))}
+            </ul>
+            {startIndex >= displayCount && (
+              <button
+                onClick={handlePrev}
+                className="absolute left-0 z-10 h-full bg-gradient-to-r from-primary to-transparent w-40 grid justify-start items-center pl-2"
+              >
+                <ChevronLeftIcon className="w-10 h-10 cursor-pointer opacity-0 group-hover:opacity-70" />
+              </button>
+            )}
+            {startIndex + displayCount < movies.length && (
+              <button
+                onClick={handleNext}
+                className="absolute right-0 h-full w-40 bg-gradient-to-r from-transparent to-primary grid justify-end items-center pr-2"
+              >
+                <ChevronRightIcon className="w-10 h-10 cursor-pointer opacity-0 group-hover:opacity-70" />
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
---
+++ core/components/media/poster.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import Image from 'next/image'
+import { useQuery } from 'react-query'
+
+import { KEY } from '@/core/enums'
+import get from '@/core/libraries'
+import { Image as PosterImage } from '@/core/types/data'
+import { IdProp } from '@/core/types/react'
+
+const Poster = ({ id }: IdProp) => {
+  const { data: poster, isFetching } = useQuery<PosterImage>({
+    queryKey: [KEY.POSTER, id],
+    queryFn: async () => await get.movie.poster({ id }),
+  })
+
+  if (isFetching || !poster) return <div className="aspect-square bg-gray-500 animate-pulse rounded-md" />
+
+  return (
+    <Image
+      alt="Poster"
+      src={`${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}${poster?.file_path}`}
+      width={poster.width}
+      height={poster.height}
+      className="rounded-md"
+    />
+  )
+}
+
+export default Poster
