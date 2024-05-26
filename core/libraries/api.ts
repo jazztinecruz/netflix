e73b1b5180f2fefa465f724034969add9ec0ceb2
jazztinecruz <me@jazztinecruz.com>
2024-05-26 12:22:06 +0800

    refactor: Update dynamic import to use static import

--- core/libraries/api.ts
+++ core/libraries/api.ts
@@ -1,15 +1,26 @@
 import axios from 'axios'
 
+import grabError from './error'
+
 type Props = {
   url: string
 }
 
-const api = async ({ url: getUrl }: Props) => {
+const api = async ({ url }: Props) => {
   try {
-    const { data } = await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/api/get-movies`, { getUrl })
+    const requestUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/api/get-movies`
+    const { data } = await axios.post(
+      requestUrl,
+      { url },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    )
     return data
   } catch (error) {
-    console.log('error', error)
+    grabError(error)
   }
 }
 export default api
