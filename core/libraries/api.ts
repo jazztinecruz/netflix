220ee20efed4e06ba0c607b1b566e392b4ef98a2
jazztinecruz <me@jazztinecruz.com>
2024-05-26 18:27:00 +0800

    refactor: Optimize API request URL handling in get-movies route

--- core/libraries/api.ts
+++ core/libraries/api.ts
@@ -8,7 +8,9 @@ type Props = {
 
 const api = async ({ url }: Props) => {
   try {
+    console.log(1, url)
     const requestUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/api/get-movies`
+    console.log(2, requestUrl, url)
     const { data } = await axios.post(
       requestUrl,
       { url },
@@ -18,6 +20,7 @@ const api = async ({ url }: Props) => {
         },
       },
     )
+    console.log(11, data)
     return data
   } catch (error) {
     grabError(error)
