15e8ae07c88d4d99c657052d4b1b8ffa11c479ae
jazztinecruz <me@jazztinecruz.com>
2024-05-28 19:13:50 +0800

    refactor: Improve error handling in API request for get-movies route

--- core/libraries/api.ts
+++ core/libraries/api.ts
@@ -11,17 +11,21 @@ const api = async ({ url }: Props) => {
     console.log(1, url)
     const requestUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/api/get-movies`
     console.log(2, requestUrl, url)
-    const { data } = await axios.post(
-      requestUrl,
-      { url },
-      {
-        headers: {
-          'Content-Type': 'application/json',
+    try {
+      const { data } = await axios.post(
+        requestUrl,
+        { url },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
         },
-      },
-    )
-    console.log(11, data)
-    return data
+      )
+      console.log(11, data)
+      return data
+    } catch (error) {
+      console.log('request post method in api', error)
+    }
   } catch (error) {
     grabError(error)
   }
