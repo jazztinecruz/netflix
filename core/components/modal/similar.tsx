790156c1d7e84dff6f253e45f61e18c68a41c32f
jazztinecruz <me@jazztinecruz.com>
2024-05-29 19:02:46 +0800

    refactor: Add expand/collapse functionality to Similar component in modal

--- core/components/modal/similar.tsx
+++ core/components/modal/similar.tsx
@@ -1,12 +1,15 @@
 'use client'
 
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/outline'
 import { useSearchParams } from 'next/navigation'
+import { useState } from 'react'
 
 import { KEY } from '@/core/enums'
 import useCustomQuery from '@/core/hook/custom-query'
 import get from '@/core/libraries'
 import { Movie } from '@/core/types/data'
 
+import Symbol from '../symbol'
 import Card from './card'
 import Group from './group'
 
@@ -25,12 +28,25 @@ const Similar = () => {
     enabled: !!mid,
   })
 
+  const [expand, setExpand] = useState(false)
+
   if (!similar || !similar.length) return null
+  const cutOffCount = expand ? similar.length : 6
 
   return (
     <Group>
       <Group.Title>More Like This</Group.Title>
-      <Group.List>{similar?.map((movie) => <Card key={movie.id} movie={movie} />)}</Group.List>
+      <Group.List>{similar?.slice(0, cutOffCount).map((movie) => <Card key={movie.id} movie={movie} />)}</Group.List>
+
+      <div className="relative grid">
+        <button
+          onClick={() => setExpand((prev) => !prev)}
+          className="grid z-10 place-items-center bg-secondary/10 border border-secondary rounded-full p-1.5 mx-auto hover:border-white"
+        >
+          <Symbol Icon={expand ? ChevronDownIcon : ChevronUpIcon} />
+        </button>
+        <div className="border w-full absolute inset-y-2/4 translate-y-2/4 border-secondary" />
+      </div>
     </Group>
   )
 }
